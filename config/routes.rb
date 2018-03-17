22bbfad42cca56b57f9df64518e5f8c5f8202ff2
Tommy_Tan <sage.ts920126@gmail.com>
2018-03-17 20:38:44 +0800

    add admin route & controller & view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources :products, only: [:index, :show]
   root "products#index"
+
+  namespace :admin do
+    resources :products
+    root "products#index"
+  end
 
 end
