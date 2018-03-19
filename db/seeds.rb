4cf3c2fee5c31627c5ed6630d7434ed0fa7a8446
Tommy_Tan <sage.ts920126@gmail.com>
2018-03-19 08:43:31 +0800

    add default admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# default admin
+User.create(email:"root@example.com", password:"123456", role:"admin")
+puts "Default admin created"
