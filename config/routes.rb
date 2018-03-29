058092998979ce89785c567ddf85840c3c0ef7d2
Tommy_Tan <sage.ts920126@gmail.com>
2018-03-29 16:14:16 +0800

    setup routes for adjust item and remove item

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "products#index"
+
   resources :products, only: [:index, :show] do
     post :add_to_cart, on: :member
+    post :remove_from_cart, on: :member
+    post :adjust_item, on: :member
   end
-  
-  root "products#index"
+
+
 
   namespace :admin do
     resources :products
