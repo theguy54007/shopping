6ac2e74093ad7286d84c1986bc34a13332a171a4
Tommy_Tan <sage.ts920126@gmail.com>
2018-03-29 18:10:47 +0800

    make correction( product -> product.id)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,12 +3,12 @@ class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   def add_cart_item(product)
-    existing_item = self.cart_items.find_by(product_id: product)
+    existing_item = self.cart_items.find_by(product_id: product.id)
     if existing_item
       existing_item.quantity += 1
       existing_item.save!
     else
-      cart_item = cart_items.build(product_id: product)
+      cart_item = cart_items.build(product_id: product.id)
       cart_item.save!
     end
     self.cart_items
