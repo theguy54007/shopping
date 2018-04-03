36312524d9f31a167407d5b3fd8ae4d26559edd8
Tommy_Tan <sage.ts920126@gmail.com>
2018-04-03 17:45:09 +0800

    setup  for after signed up path when create new order from cart-show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+
+  # redirect to the form if there is a form_data in the session
+  if session[:form_data].present?
+
+    return cart_path#redirect to your form path
+
+  else
+    #if there is not temp list in the session proceed as normal
+    super
+  end
+
+end
+
+
+
 
   def authenticate_admin
     unless current_user.admin?
