192e86ea72799bf216543e47038a25626836de8f
Tommy_Tan <sage.ts920126@gmail.com>
2018-04-03 17:52:14 +0800

    add creating order funciton & index function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,43 @@
 class OrdersController < ApplicationController
+
+  def index
+      @orders = Order.all
+  end
+
+
+
+
+  def create
+    if current_user.nil?
+      session[:form_data] = params[:order]
+      redirect_to new_user_session_path
+    else
+      @order = Order.new(order_params)
+      @order.user_id = current_user.id
+      @order.shipping_status = "Not shipped!"
+      @order.payment_status = "Not Paid!"
+      @order.sn = 1000 + Order.count
+      @order.amount = 0
+      @cart_items = current_cart.cart_items.all
+      if @order.save
+        @cart_items.each do |item|
+          order_item = @order.order_items.build(product_id: item.product.id, price: item.product.price, quantity: item.quantity)
+          @order.amount += item.product.price * item.quantity
+          order_item.save!
+        end
+        @order.save!
+        session[:cart_id] = nil
+        redirect_to orders_path
+      else
+        render "carts/show.html.erb"
+      end
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :phone, :address)
+  end
+
 end
