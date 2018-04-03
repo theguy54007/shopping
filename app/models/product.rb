7f49603ccb7fd4d342945a7a6b843f3ebff0a023
Tommy_Tan <sage.ts920126@gmail.com>
2018-04-03 21:17:53 +0800

    make correction ( delete unuse code)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :image, PhotoUploader
-  validates_presence_of :name, :price,
-  validates_presence_of :price
+  validates_presence_of :name, :price
+  
 
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
