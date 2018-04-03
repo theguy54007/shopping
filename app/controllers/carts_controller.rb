2445ed116d539cd95bc90b20143d3b9aee3e6fd8
Tommy_Tan <sage.ts920126@gmail.com>
2018-04-03 17:45:50 +0800

    add new order and save form data after login

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,6 +3,11 @@ class CartsController < ApplicationController
   def show
     @cart = current_cart
     @items = @cart.cart_items
+    if session[:form_data].present?
+      @order = Order.new(session[:form_data])
+    else
+      @order = Order.new
+    end
   end
 
 end
