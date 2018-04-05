ff9691b515ed733b629d409c6235ef3320003096
Tommy_Tan <sage.ts920126@gmail.com>
2018-04-05 09:18:02 +0800

    sent mails to customer after admin change payment_status or shipping_status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,9 +10,19 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to admin_orders_path
-    flash[:notice] = "Order was updated!"
+    if @order.update(order_params)
+      if @order.payment_status == "Paid"
+        UserMailer.notify_order_paid(@order).deliver_now!
+      end
+      if @order.shipping_status == "Shipped"
+        UserMailer.notify_order_shipped(@order).deliver_now!
+      end
+      redirect_to admin_orders_path
+      flash[:notice] = "Order was updated!"
+    else
+      flash.now[:alert] = @order.errors.full_messages.to_sentence
+      render "admin/orders/edit"
+    end
   end
 
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,5 +9,22 @@ class UserMailer < ApplicationMailer
     subject: "ALPHA Camp | 訂單成立：#{@order.sn}"
   end
 
+  def notify_order_paid(order)
+    @order = order
+    @content = "Your order has been paid. Thank you!"
+
+    mail to: order.user.email,
+    subject: "Order# #{@order.sn} has been paid!"
+  end
+
+  def notify_order_shipped(order)
+    @order = order
+    @content = "Your order has been shipped. Thank you!"
+
+    mail to: order.user.email,
+    subject: "Order# #{@order.sn} has been shipped!"
+  end
+
+
 
 end
