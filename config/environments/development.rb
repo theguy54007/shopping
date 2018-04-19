743399ef12c023b73d35c4d63b045c483d5d4ef7
Tommy <sage.ts920126@gmail.com>
2018-04-19 08:06:31 +0800

    make gmail work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "https://shopping-store-online.herokuapp.com/" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   config.action_mailer.perform_caching = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,25 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'https://shopping-store-online.herokuapp.com/' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "myapp.herokuapp.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["sage.tommy920126@gmail.com"],
+  password: ENV["920126tommy"]
+}
+
+
+
+
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
